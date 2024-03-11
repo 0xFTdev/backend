ca6de3b83a755495ae66bc2f92fed7daa566747e
digitalflowerstudio <floriantappeser@gmail.com>
2024-03-11 11:16:50 +0100

    test

--- mongodb_express/backend/index.js
+++ mongodb_express/backend/index.js
@@ -10,6 +10,8 @@ const app = express();
 
 // app.use(cors());
 // app.use("/public", express.static("./public"))
+
+// GET http://localhost:3000/expenses
 app.use("/expenses", expensesRouter);
 
 app.listen(PORT, () => {
